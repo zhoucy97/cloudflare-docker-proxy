c5dc687e6d4724f92954b2b588416b39fefeac25
zhou chang yu <zhouchangyu@88.com>
2024-06-27 12:17:43 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-addEventListener("fetch", (event) => {
+addEventListener("fetch"， (event) => {
   event.passThroughOnException();
   event.respondWith(handleRequest(event.request));
 });
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.599997.xyz": dockerHub,
+  "quay.599997.xyz": "https://quay.io",
+  "gcr.599997.xyz": "https://gcr.io",
+  "k8s-gcr.599997.xyz": "https://k8s.gcr.io",
+  "k8s.599997.xyz": "https://registry.k8s.io",
+  "ghcr.599997.xyz": "https://ghcr.io",
+  "cloudsmith.599997.xyz": "https://docker.cloudsmith.io",
+  "ecr.599997.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.599997.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
